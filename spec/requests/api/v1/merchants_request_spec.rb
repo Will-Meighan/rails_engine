122b05120eb3dee74d81ece1bd71165d11b8cebb
William Meighan <williamdmeighan@gmail.com>
2020-03-23 07:18:31 -0600

    add test for most revenue

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -108,4 +108,32 @@ describe 'Merchants API' do
 
     expect(json[:data].length).to eq(5)
   end
+
+  it 'can get merchants with the most revenue' do
+    merchant_1 = create(:merchant, name: 'Wills toyshop')
+    merchant_2 = create(:merchant, name: 'megs bikeshop')
+
+    invoice_items_1 = create_list(:invoice_item, 3, quantity: 1, unit_price: 50)
+    invoice_items_2 = create_list(:invoice_item, 2, quantity: 2, unit_price: 30)
+
+    invoice_items_1.each do |invoice_item|
+      invoice_item.invoice.update(merchant_id: merchant_1.id)
+      create(:transaction, invoice: invoice_item.invoice, result: 'success')
+    end
+
+    invoice_items_2.each do |invoice_item|
+      invoice_item.invoice.update(merchant_id: merchant_2.id)
+      create(:transaction, invoice: invoice_item.invoice, result: 'success')
+    end
+
+    get '/api/v1/merchants/most_revenue?quantity=2'
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data].count).to eq(2)
+    expect(json[:data].first[:attributes][:name]).to eq('megs bikeshop')
+    expect(json[:data].last[:attributes][:name]).to eq('Wills toyshop')
+  end
 end
